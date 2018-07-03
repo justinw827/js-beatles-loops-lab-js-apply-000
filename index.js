4e166000782025ae3ade7c54f800dfdf948158b9
Justin Wang <justinw827@aol.com>
2018-07-03 01:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0;
   while (i < facts.length) {
-    facts[i] = facts[i] + '!';
+    facts[i] = facts[i] + "!!!";
     i++;
   }
 }
